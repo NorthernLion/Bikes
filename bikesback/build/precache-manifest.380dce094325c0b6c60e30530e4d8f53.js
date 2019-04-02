375a696913fb031ba918c8147a3decfcb983a46b
NorthernLion <joppetaja@gmail.com>
2019-04-02 14:36:46 +0300

    Gitignored build acidentally...

---
+++ bikesback/build/precache-manifest.380dce094325c0b6c60e30530e4d8f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be188c1b030a01d1588",
+    "url": "/static/css/main.53405f99.chunk.css"
+  },
+  {
+    "revision": "6be188c1b030a01d1588",
+    "url": "/static/js/main.2dc18e32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "beba6acbd9a9d110d2ae",
+    "url": "/static/js/2.30c6a86c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5793792f4776d92ea678b1fb26a96593",
+    "url": "/index.html"
+  }
+];
