94b7c9944b7aa70bd9df6e9280cb3f189179d0e0
NorthernLion <joppetaja@gmail.com>
2019-04-09 11:33:28 +0300

    Apollo connection works

--- bikesfront/src/index.js
+++ bikesfront/src/index.js
@@ -6,12 +6,20 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './util/store'
+import client from './util/apollo'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+
 
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <App />
+        </ApolloHooksProvider>
+      </ApolloProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
---
+++ bikesfront/src/util/apollo.js
@@ -0,0 +1,24 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+
+const httpLink = createHttpLink({
+  uri: 'https://www.hsl.fi/en/citybikes',
+})
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+export default client
